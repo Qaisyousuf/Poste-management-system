09ab188e86cc38efe6609dced97ca2e2b50cedc5
Qais Yousuf <mr.qais.yousuf@gmail.com>
2020-03-17 09:29:23 +0100

    MDBootstrapInstalled

--- OPMS.Web/App_Start/BundleConfig.cs
+++ OPMS.Web/App_Start/BundleConfig.cs
@@ -19,11 +19,13 @@ namespace OPMS.Web
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/mdb.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
+                      "~/Content/mdb.min.css",
                       "~/Content/CustomPostCSS.css"
                      ));
 
